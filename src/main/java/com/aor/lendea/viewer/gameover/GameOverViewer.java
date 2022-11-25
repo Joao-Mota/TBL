9abfd8ba28b0085e297c3e12740aafd1cbcadf4b
Pedro Landolt <pedrolandolt88@gmail.com>
2022-11-25 11:27:18 +0000

    Help Menu Implemented

--- src/main/java/com/aor/lendea/viewer/gameover/GameOverViewer.java
+++ src/main/java/com/aor/lendea/viewer/gameover/GameOverViewer.java
@@ -12,11 +12,21 @@ public class GameOverViewer extends Viewer<GameOver> {
 
     @Override
     public void drawElements(GUI gui) {
-        gui.drawText(new Position(35, 3), "GAME OVER", "#FFA212");
+
+        gui.drawText(new Position(4, 1), "  ####                                     ####                            " , "#FFA212");
+        gui.drawText(new Position(4, 2), " #######                                  #######                          " , "#FFA212");
+        gui.drawText(new Position(4, 3), "####      ######    ##### ###    ###     ###  ###  ##  ###   ###    ####   " , "#FFA212");
+        gui.drawText(new Position(4, 4), "###      ######    ##########  ######    ###   ##  ## ###  ######  ######  " , "#FFA212");
+        gui.drawText(new Position(4, 5), "##  ###  ### ##    ### ### ##  ##  ##    ##    ##  ## ##   ##  ##  ###     " , "#FFA212");
+        gui.drawText(new Position(4, 6), "## ####  ##  ##    ##  ##  ##  ######    ##   ###  #####   ######  ##      " , "#FFA212");
+        gui.drawText(new Position(4, 7), "##   ##  ## ###    ##  ##  ##  ###       ### ###   ####    ###     ##      " , "#FFA212");
+        gui.drawText(new Position(4, 8), "#######  #######  ### ### ###  #####     #######   ####    #####  ###      " , "#FFA212");
+        gui.drawText(new Position(4, 9), " #####   ##### ## ### ### ###   ####      ####     ###      ####  ###      " , "#FFA212");
+
 
         for (int i = 0; i < getModel().getNumberEntries(); i++)
             gui.drawText(
-                    new Position(5, 13 + i),
+                    new Position(55, 17 + i),
                     getModel().getEntry(i),
                     getModel().isSelected(i) ? "#FFA212" : "#FFFFFF");
     }
