84d53c2af4e8496490557cc5e174dd8efaae7dd0
Pedro Landolt <pedrolandolt88@gmail.com>
2022-12-09 14:21:51 +0000

    Lvl1 Finished

--- src/main/java/pt/up/fe/lendea/viewer/layout/GameViewer.java
+++ src/main/java/pt/up/fe/lendea/viewer/layout/GameViewer.java
@@ -23,7 +23,7 @@ public class GameViewer extends Viewer<Arena> {
         drawElements(gui, getModel().getShots(), new ShotViewer());
         drawElements(gui, getModel().getBossShots(), new BossShotViewer());
 
-        gui.drawText(new Position(69, 4), "LEVEL", "#FFFFFF");
+
         gui.drawText(new Position(66, 20), "HP: " + getModel().getLendea().getHealth() + " &", "#FA1616");
         gui.drawText(new Position(66, 24), "COINS: " + getModel().getLendea().getCoins() + " $", "#F6C345");
         gui.drawText(new Position(66, 39), "TIMER: " + getModel().getTimer().getSeconds(), "#FFFFFF");
